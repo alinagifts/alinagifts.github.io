fb3d442d5d931e1ca47511f41951525272b12031
alinagifts <alinagifts.art@gmail.com>
2017-11-25 05:26:31 +0200

    test php

--- mail.php
+++ mail.php
@@ -1,49 +1,8 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-if (!empty($_POST['name']) && !empty($_POST['phone'])){
-  if (isset($_POST['name'])) {
-    if (!empty($_POST['name'])){
-  $name = strip_tags($_POST['name']);
-  $nameFieldset = "Имя пославшего: ";
-  }
-}
- 
-if (isset($_POST['phone'])) {
-  if (!empty($_POST['phone'])){
-  $phone = strip_tags($_POST['phone']);
-  $phoneFieldset = "Телефон: ";
-  }
-}
-if (isset($_POST['theme'])) {
-  if (!empty($_POST['theme'])){
-  $theme = strip_tags($_POST['theme']);
-  $themeFieldset = "Тема: ";
-  }
-}
-$token = "415769805:AAF_B_UClO0zgP1TAZ4_s4Ra6kRD-_3ELKo";
+ <?php
+$botToken = "415769805:AAF_B_UClO0zgP1TAZ4_s4Ra6kRD-_3ELKo";
 $chat_id = "-221520487";
-$arr = array(
-  $nameFieldset => $name,
-  $phoneFieldset => $phone,
-  $themeFieldset => $theme
-);
-foreach($arr as $key => $value) {
-  $txt .= "<b>".$key."</b> ".$value."%0A";
-};
-$sendToTelegram = fopen("https://translate.google.com","r");
-//$homepage = file_get_contents("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}");
-if ($sendToTelegram) {
-  
-  echo '<p class="success">Спасибо за отправку вашего сообщения!</p>';
-    return true;
-} else {
-  echo '<p class="fail"><b>Ошибка. Сообщение не отправлено!</b></p>';
-}
-} else {
-  echo '<p class="fail">Ошибка. Вы заполнили не все обязательные поля!</p>';
-}
-} else {
-header ("Location: /");
-}
- 
+$message = "heeeeey";
+$bot_url    = "https://api.telegram.org/bot$botToken/";
+$url = $bot_url."sendMessage?chat_id=".$chat_id."&text=".urlencode($message);
+file_get_contents($url);
 ?>
