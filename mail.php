aa8f0322133c74425e6036ee88a51019000f4348
[LenaShy] <[elena.shynkarenko@gmail.com]>
2017-12-01 20:43:09 +0200

    add feedback form

--- mail.php
+++
@@ -1,8 +0,0 @@
- <?php
-$botToken = "415769805:AAF_B_UClO0zgP1TAZ4_s4Ra6kRD-_3ELKo";
-$chat_id = "-221520487";
-$message = "heeeeey";
-$bot_url    = "https://api.telegram.org/bot$botToken/";
-$url = $bot_url."sendMessage?chat_id=".$chat_id."&text=".urlencode($message);
-file_get_contents($url);
-?>
